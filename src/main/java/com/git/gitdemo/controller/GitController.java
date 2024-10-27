011407a4d6ae8d6b450b9d8186a044b27d14dec5
omprakash gutta <prakashgutta186@gmail.com>
2024-10-27 14:12:52 +0530

    my second trail

---
+++ src/main/java/com/git/gitdemo/controller/GitController.java
@@ -0,0 +1,14 @@
+package com.git.gitdemo.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GitController {
+
+    public String hello(){
+
+
+        return " hello spring ";
+        }
+
+}
